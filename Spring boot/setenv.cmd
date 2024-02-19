4c964ca3c3a7a07fc1712aadb2eed7e4f8cea2b1
maeen <maeenuddin1971@gmail.com>
2024-02-19 14:51:24 +0800

    Files are commited

---
+++ Spring boot/setenv.cmd
@@ -0,0 +1,12 @@
+
+set JAVA_HOME=C:\opt\jdk-21
+set JAVA_HOME=C:\opt\jdk-17
+set ANT_HOME=C:\opt\ant
+set MAVEN_HOME=C:\opt\maven
+
+set PWD=D:\Privasia\BlessProject\PaymentAgent\paynet-rpp
+
+set CLASSPATH=%PWD%\target\test-classes;%PWD%\target\classes;.;%PWD%\runtime\*;%PWD%\target\paynet-1.0\WEB-INF\lib\*
+
+set PATH=%JAVA_HOME%\bin;%ANT_HOME%\bin;%MAVEN_HOME%\bin;C:\Windows\system32;C:\Windows\System32\OpenSSH;C:\Windows;C:\Program Files\Git\cmd;
+
